0394b7ef4404b5a16395504f48c4d5fbb30462aa
Wylx-32k <iwylxmci@gmail.com>
2025-12-31 07:45:31 -0800

    Add DLL archive, change UI.

--- HL+/Form1.Designer.cs
+++ HL+/Form1.Designer.cs
@@ -29,77 +29,91 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            LBGray = new Panel();
+            LBWhite = new Panel();
+            Splash = new Label();
             label2 = new Label();
-            panel1 = new Panel();
-            label3 = new Label();
+            LBGray.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.Font = new Font("Segoe UI Black", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
             label1.ForeColor = Color.White;
-            label1.Location = new Point(0, 0);
+            label1.Location = new Point(0, 21);
             label1.Name = "label1";
             label1.Size = new Size(300, 44);
             label1.TabIndex = 0;
             label1.Text = "Horion Launcher";
             label1.TextAlign = ContentAlignment.MiddleCenter;
             // 
+            // LBGray
+            // 
+            LBGray.BackColor = Color.FromArgb(30, 30, 30);
+            LBGray.Controls.Add(LBWhite);
+            LBGray.Location = new Point(10, 101);
+            LBGray.Name = "LBGray";
+            LBGray.Size = new Size(280, 2);
+            LBGray.TabIndex = 2;
+            // 
+            // LBWhite
+            // 
+            LBWhite.BackColor = Color.FromArgb(199, 199, 199);
+            LBWhite.ForeColor = SystemColors.Control;
+            LBWhite.Location = new Point(0, 0);
+            LBWhite.Name = "LBWhite";
+            LBWhite.Size = new Size(10, 2);
+            LBWhite.TabIndex = 3;
+            // 
+            // Splash
+            // 
+            Splash.Font = new Font("Open Sans", 8.25F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            Splash.ForeColor = Color.White;
+            Splash.Location = new Point(0, 100);
+            Splash.Name = "Splash";
+            Splash.Size = new Size(300, 22);
+            Splash.TabIndex = 3;
+            Splash.Text = "Source code publicly available on GitHub!";
+            Splash.TextAlign = ContentAlignment.MiddleCenter;
+            // 
             // label2
             // 
             label2.Font = new Font("Segoe UI Semibold", 10F, FontStyle.Italic);
             label2.ForeColor = Color.LightGray;
-            label2.Location = new Point(0, 35);
+            label2.Location = new Point(0, 65);
             label2.Name = "label2";
             label2.Size = new Size(300, 31);
             label2.TabIndex = 1;
-            label2.Text = "With version history!";
+            label2.Text = "v1.0b";
             label2.TextAlign = ContentAlignment.MiddleCenter;
             label2.Click += label2_Click;
-            // 
-            // panel1
-            // 
-            panel1.BackColor = Color.White;
-            panel1.Location = new Point(10, 120);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(280, 1);
-            panel1.TabIndex = 2;
-            // 
-            // label3
-            // 
-            label3.Font = new Font("Open Sans", 8F);
-            label3.ForeColor = Color.White;
-            label3.Location = new Point(0, 120);
-            label3.Name = "label3";
-            label3.Size = new Size(300, 30);
-            label3.TabIndex = 3;
-            label3.Text = "You can find the source code here.";
-            label3.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(25, 25, 50);
-            ClientSize = new Size(300, 150);
-            Controls.Add(panel1);
-            Controls.Add(label3);
+            BackColor = Color.FromArgb(20, 20, 25);
+            ClientSize = new Size(300, 128);
+            Controls.Add(LBGray);
             Controls.Add(label2);
             Controls.Add(label1);
+            Controls.Add(Splash);
             ForeColor = SystemColors.ControlText;
             FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
             Load += Form1_Load;
+            LBGray.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
 
         private Label label1;
+        private Panel LBGray;
+        private Label Splash;
+        private Panel LBWhite;
         private Label label2;
-        private Panel panel1;
-        private Label label3;
     }
 }
--- HL+/Form1.cs
+++ HL+/Form1.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Net;
+
 namespace HL_
 {
     public partial class Form1 : Form
@@ -14,7 +17,73 @@ namespace HL_
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            int maxWidth = 280;
+            double duration = 3000;
 
+            var sw = Stopwatch.StartNew();
+            var timer = new System.Windows.Forms.Timer();
+            timer.Interval = 16;
+
+            timer.Tick += (s, e2) =>
+            {
+                double t = sw.Elapsed.TotalMilliseconds / duration;
+                if (t >= 1)
+                {
+                    LBWhite.Width = maxWidth;
+                    timer.Stop();
+                    return;
+                }
+
+                double eased = 1 - Math.Pow(1 - t, 3);
+                LBWhite.Width = (int)(eased * maxWidth);
+
+                if (t > 0.33 && t < 0.35)
+                {
+                    if (!CheckForInternetConnection())
+                    {
+                        MessageBox.Show("This application requires internet, please re-launch when connected.");
+                        Application.Exit();
+                    }
+                }
+
+                if (t > 0.66 && t < 0.68)
+                {
+
+                    if (CheckForInternetConnection())
+                    {
+
+                    }
+                    else { Application.Exit(); }
+                }
+
+                if (t > 0.9 && t < 0.92)
+                {
+                    if (CheckForInternetConnection())
+                    {
+
+                    }
+                    else { Application.Exit(); }
+                }
+            };
+
+            timer.Start();
+        }
+
+        //too lazy to code ty stack overflow
+        private static bool CheckForInternetConnection()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                using (var stream = client.OpenRead("http://www.google.com"))
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+                return false;
+            }
         }
     }
 }
